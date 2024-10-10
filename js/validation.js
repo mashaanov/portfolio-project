80c10993825270297879eccf2910b5ca5f6dbcf5
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-10-10 21:51:56 +0400

    fix validation logic

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,34 @@
-import * as yup from 'https://cdn.jsdelivr.net/npm/yup@1/+esm';
+import * as yup from "https://cdn.jsdelivr.net/npm/yup@1/+esm";
 
 const schema = yup.object().shape({
-    fullName: yup.string().matches(/^[a-zA-Zа-яА-ЯёЁ\s-]+$/, 'Invalid fullName format').required('Full Name is required'),
-    email: yup.string().matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Invalid email format').required('Email is required'),
-    phone: yup.string().matches(/^\+\d{1,3}\s\d{4}\s\d{2}\s\d{2}\s\d{2}$/, 'Invalid phone format. The format should be: +X (XXX) XXX-XX-XX').required('Phone is required'),
-    subject: yup.string().matches(/^[a-zA-Zа-яА-ЯёЁ\s-]+$/).required('Subject is required'),
-    message: yup.string().matches(/^[a-zA-Zа-яА-ЯёЁ\s-]$/),
-})
+  fullName: yup
+    .string()
+    .matches(/^[a-zA-Zа-яА-ЯёЁ\s-]+$/, "Full Name format is invalid")
+    .required("Full Name is required"),
+  email: yup
+    .string()
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Email format is invalid"
+    )
+    .required("Email is required"),
+  phone: yup
+    .string()
+    .matches(
+      /^\+\d{1,3}\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}$/,
+      "Phone format is invalid"
+    )
+    .required("Phone is required"),
+  subject: yup
+    .string()
+    .matches(/^[a-zA-Zа-яА-ЯёЁ\s-]*$/, "Subject format is invalid")
+    .required("Subject is required"),
+  message: yup
+    .string()
+    .required(false)
+    .matches(/^[a-zA-Zа-яА-ЯёЁ\s-?!:)(')]*$/, "Message format is invalid"),
+});
 
 export default (formData) => {
-    return schema.validate(formData, { abortEarly: false });
+  return schema.validate(formData, { abortEarly: false });
 };
