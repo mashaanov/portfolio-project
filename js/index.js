395a427aac0c51d57224939a98d2addd6579a9c4
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-10-10 21:49:43 +0400

    introduce mvc model, add phone mask, improve logic

--- js/index.js
+++ js/index.js
@@ -1,87 +1,104 @@
+import Model from "./model.js";
+import View from "./view.js";
 import gsapAnimation from "./js-gsap.js";
-import validate from "./validation.js";
+import { maskList } from "./mask-list.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  gsapAnimation();
-  emailjs.init("MvRAODkHqNCcghXiU");
-});
+class Controller {
+  constructor() {
+    this.model = new Model();
+    this.view = new View();
 
-const validateAndSentForm = async (formData) => {
-  try {
-    await validate(formData);
+    this.initialize();
+  }
 
-    await emailjs.sendForm(
-      "service_9wg442l",
-      "template_z668k4e",
-      document.getElementById("contact-form")
-    );
-    alert("Successfully sent!");
-    document.getElementById("contact-form").reset();
-  } catch (error) {
-    document
-      .querySelectorAll(".error-message")
-      .forEach((el) => (el.textContent = ""));
+  initialize() {
+    document.addEventListener("DOMContentLoaded", () => {
+      gsapAnimation();
+      emailjs.init("MvRAODkHqNCcghXiU");
 
-    if (error.name === "ValidationError") {
-      error.errors.forEach((err) => {
-        if (
-          err === "Full Name is required" ||
-          err.startsWith("Invalid full name")
-        ) {
-          const errorMessage = document.getElementById("fullNameError");
-          errorMessage.textContent = err;
-        } else if (
-          err === "Email is required" ||
-          err.startsWith("Invalid email")
-        ) {
-          const errorMessage = document.getElementById("emailError");
-          errorMessage.textContent = err;
-        } else if (
-          err === "Phone is required" ||
-          err.startsWith("Invalid phone")
-        ) {
-          const errorMessage = document.getElementById("phoneError");
-          errorMessage.textContent = err;
-        } else if (
-          err === "Subject is required" ||
-          err.startsWith("Invalid subject")
-        ) {
-          const errorMessage = document.getElementById("subjectError");
-          errorMessage.textContent = err;
-        } else if (
-          err === "Message is required" ||
-          err.startsWith("Invalid message")
-        ) {
-          const errorMessage = document.getElementById("messageError");
-          errorMessage.textContent = err;
+      const phoneInput = this.view.form.querySelector('[name="phone"]');
+      if (phoneInput) {
+        this.mask('[name="phone"]');
+      }
+
+      this.view.form.addEventListener("submit", (e) => {
+        this.handleSubmit(e);
+      });
+    });
+  }
+
+  mask = (selector) => {
+    const setMask = (event) => {
+      let matrix = "+###############";
+      let phone = event.target.value.replace(/[\s#-)(]/g, "");
+      maskList.forEach((item) => {
+        let code = item.code.replace(/[\s#]/g, "");
+        if (phone.startsWith(code)) {
+          matrix = item.code;
         }
       });
-    } else {
-      alert("Error during form submission: " + JSON.stringify(error));
-    }
-  }
-};
 
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const fullName = this.querySelector('input[type="text"]').value.trim();
-    const email = this.querySelector('input[type="email"]').value.trim();
-    const phone = this.querySelector('input[type="tel"]').value.trim();
-    const subject = this.querySelector(
-      'input[type="text"][placeholder="Subject"]'
-    ).value.trim();
-    const message = this.querySelector("textarea").value.trim();
-
-    const formData = {
-      fullName,
-      email,
-      phone,
-      subject,
-      message,
+      let i = 0;
+      let val = event.target.value.replace(/\D/g, "");
+      event.target.value = matrix.replace(/(?!\+)./g, function (a) {
+        return /[#\d]/.test(a) && i < val.length
+          ? val.charAt(i++)
+          : i >= val.length
+          ? ""
+          : a;
+      });
     };
 
-    validateAndSentForm(formData);
-  });
+    let inputs = document.querySelectorAll(selector);
+
+    inputs.forEach((input) => {
+      if (!input.value) input.value = "+";
+      input.addEventListener("input", setMask);
+      input.addEventListener("focus", setMask);
+      input.addEventListener("blur", setMask);
+    });
+  };
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    const formElement = this.view.form;
+
+    try {
+      const formData = this.getFormData();
+      const { valid, errors } = await this.model.isValidate(formData);
+
+      if (valid) {
+        await this.model.sendForm(formElement);
+        this.model.state.form.process.state = "success";
+        this.view.showSuccessMessage();
+        this.view.resetForm();
+      } else {
+        this.model.state.form.process.state = "failed";
+        this.view.showErrorMessage(errors);
+      }
+    } catch (error) {
+      console.log("Error during form submission:", error);
+    }
+  }
+
+  getFormData() {
+    const fullName = this.view.form
+      .querySelector('input[type="text"]')
+      .value.trim();
+    const email = this.view.form
+      .querySelector('input[type="email"]')
+      .value.trim();
+    const phone = this.view.form
+      .querySelector('input[type="tel"]')
+      .value.trim();
+    const subject = this.view.form
+      .querySelector('input[type="text"][placeholder="Subject"]')
+      .value.trim();
+    const message = this.view.form.querySelector("textarea").value.trim();
+
+    return { fullName, email, phone, subject, message };
+  }
+}
+
+new Controller();
